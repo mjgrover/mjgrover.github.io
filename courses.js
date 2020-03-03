ceec631c7eebd017daec0497ebf1a453c37198c3
MARK J GROVER <mjgrover@us.ibm.com>
2020-03-03 16:04:54 -0500

    Update courses.js

--- courses.js
+++ courses.js
@@ -4,6 +4,7 @@
 //'use strict';
 var counter = '0';
 var display_type; //1=short
+var segment_val;
 $.ajaxSetup({
 	async: false
 });
@@ -12,35 +13,54 @@ $.ajaxSetup({
 //	insert_course_card(json_file, container);
 //});
 
-function create_card(json_file, cardlocation, card_type){
-	console.log("pre:Create_card "+counter+ ",Display:"+display_type+", json:"+json_file+", Location:"+cardlocation+", Card_type:"+card_type);
+function create_card(json_file, cardlocation, card_type, card_group){
+	
 	display_type = card_type;
+	segment_val = card_group;
+	
 	insert_course_card(json_file, cardlocation);
-	console.log("create_card "+counter+ ",Display: "+display_type);
 }
 
 function insert_course_card(json_file, cardlocation){
 	$.getJSON(json_file, function(data){
-		console.log("insert_course_card "+counter+",Display:"+display_type+",Card_Location:"+cardlocation);
+		
 		var block_to_insert ;
 		var container_block ;
 		
 		$.each(data, function(key, value){
-			console.log("Display: "+display_type);
-			block_to_insert = document.createElement( 'div' );
-			block_to_insert.className = 'bx--col-sm-4 bx--col-lg-4';
-			block_to_insert.id = 'cardContainer'+counter;
+			
+			if (value.segment == segment_val){
+				
+				block_to_insert = document.createElement( 'div' );
+				block_to_insert.className = 'bx--col-sm-4 bx--col-lg-4';
+				block_to_insert.id = 'cardContainer'+counter;
 
-			container_block = document.getElementById( cardlocation );
-			container_block.appendChild( block_to_insert );
-			{
-				populate_card(value);
-				counter++;
-				console.log("cardContainer "+counter+",Display: "+display_type)
+				container_block = document.getElementById( cardlocation );
+				container_block.appendChild( block_to_insert );
+				{
+					populate_card(value);
+					counter++;
+				}	
 			}
-		});
+			else{
+				if (segment_val === undefined){
+			
+				block_to_insert = document.createElement( 'div' );
+				block_to_insert.className = 'bx--col-sm-4 bx--col-lg-4';
+				block_to_insert.id = 'cardContainer'+counter;
+
+				container_block = document.getElementById( cardlocation );
+				container_block.appendChild( block_to_insert );
+				{
+					populate_card(value);
+					counter++;
+				}
+			}
+		}});
 	});
 }
+
+
 function populate_card(value){
 
 	var block_to_insert ;
@@ -54,14 +74,13 @@ function populate_card(value){
 
 	container_block = document.getElementById( 'cardContainer'+counter );
 	container_block.appendChild( block_to_insert );
-	console.log("card "+counter+", Display: "+display_type)
 
 	// Add Href wrapper
 //	var course_url = 'www.google.com';
 	var a = document.createElement ('a');
 	a.className = 'developer--card__block_link';
 	a.id = 'href_wrapper'+counter;
-		if (value.wrapper === "true"){
+		if (value.wrapper === "y"){
 			a.href = "content_wrapper.html?loc="+value.course_url+"&code="+value.course_code;
 			}
 		else{
@@ -78,7 +97,12 @@ function populate_card(value){
 		var h5 = document.createElement ('h5');
 		h5.id = 'learning_type'+counter;
 		h5.className = 'developer--card__type';
-		h5.innerHTML = value.Learning_Type;
+		if (value.learning_type == "Instructor-led"){
+			h5.innerHTML = value.learning_type;
+		}
+		else{
+			h5.innerHTML = "eLearning | "+value.learning_type;
+		}
 
 		container_block = document.getElementById('href_wrapper'+counter);
 		container_block.appendChild( h5 );
@@ -96,7 +120,7 @@ function populate_card(value){
 				var h3 = document.createElement ('h3');
 				h3.id = 'card_title'+counter;
 				h3.className = 'developer--card__title';
-				h3.innerHTML = value.Course_Title;
+				h3.innerHTML = value.course_title;
 
 				container_block = document.getElementById( 'card_body'+counter );
 				container_block.appendChild( h3 );
@@ -108,7 +132,7 @@ function populate_card(value){
 				var p = document.createElement ('p');
 				p.id = 'card_description'+counter;
 				p.className = 'developer--card__description';
-				p.innerHTML = value.Description;
+				p.innerHTML = value.description;
 				
 				container_block = document.getElementById( 'card_body'+counter );
 				container_block.appendChild( p );
@@ -152,4 +176,73 @@ function populate_card(value){
 			container_block = document.getElementById( 'card_bottom'+counter );
 			container_block.appendChild( svgElem );
 
+}
+
+function featured_card(json_file, cardlocation){
+	$.getJSON(json_file, function(data){
+		
+		var fcounter ='0';
+		var block_to_insert ;
+		var container_block ;
+		
+		$.each(data, function(key, value){
+				block_to_insert = document.createElement( 'div' );
+				block_to_insert.className = 'bx--col-md-4 bx--col-lg-4';
+				block_to_insert.id = 'cardContainer'+fcounter;
+
+				container_block = document.getElementById( cardlocation );
+				container_block.appendChild( block_to_insert );
+			
+				var h2 = document.createElement ('h2');
+				h2.id = 'featured'+fcounter;
+				h2.innerHTML = "Featured Course: ";
+				
+				container_block = document.getElementById('cardContainer'+fcounter);
+				container_block.appendChild( h2 );
+			
+				var h3 = document.createElement ('h3');
+				h3.id = 'card_title'+fcounter;
+				h3.className = 'code--taxonomy__section-header';
+				h3.innerHTML = value.course_title;
+
+				container_block = document.getElementById( 'cardContainer'+fcounter );
+				container_block.appendChild( h3 );
+			
+				var p = document.createElement ('p');
+				p.id = 'wrapper'+fcounter;
+				
+				container_block = document.getElementById( 'cardContainer'+fcounter );
+				container_block.appendChild( p );
+			
+				var a = document.createElement ('a');
+				a.id = 'href_wrapper'+fcounter;
+					if (value.wrapper === "y"){
+						a.href = "content_wrapper.html?loc="+value.course_url+"&code="+value.course_code;
+						}
+					else{
+						a.href = "https://"+value.course_url;
+					}
+				a.target = '_blank';
+				a.innerHTML = 'Access Course &#187;';
+
+				container_block = document.getElementById( 'wrapper'+fcounter );
+				container_block.appendChild( a );
+			
+			
+				block_to_insert = document.createElement( 'div' );
+				block_to_insert.className = 'bx--col-md-8 bx--col-lg-8'
+				block_to_insert.id = 'description'+fcounter;
+
+				container_block = document.getElementById( cardlocation );
+				container_block.appendChild( block_to_insert );
+			
+				p = document.createElement ('p');
+				p.id = 'card_description'+fcounter;
+				p.innerHTML = value.description;
+				
+				container_block = document.getElementById( 'description'+fcounter );
+				container_block.appendChild( p );
+			
+		});
+	});
 }
